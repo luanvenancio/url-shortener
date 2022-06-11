e9bd7ac482512f59f401b781d00b4f1b6e58d844
Luan <j0hn04v@gmail.com>
2022-06-11 19:33:25 -0300

    Initial commit

---
+++ src/controller/URLController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import shortId from "shortid";
+import { URLModel } from "../database/model/URL";
+
+require("dotenv").config();
+
+export class URLController {
+    public async shorten(req: Request, res: Response): Promise<void> {
+        const { originURL } = req.body;
+
+        const url = await URLModel.findOne({ originURL });
+        if (url) {
+            res.json(url);
+            return;
+        }
+        const hash = shortId.generate();
+        const shortURL = `${process.env.API_URL}/${hash}`;
+        const newURL = await URLModel.create({ hash, shortURL, originURL });
+
+        res.json(newURL);
+    }
+
+    public async redirect(req: Request, res: Response): Promise<void> {
+        const { hash } = req.params;
+        const url = await URLModel.findOne({ hash });
+
+        if (url) {
+            res.redirect(url.originURL);
+            return;
+        }
+
+        res.status(400).json({ error: "URL not found" });
+    }
+}
